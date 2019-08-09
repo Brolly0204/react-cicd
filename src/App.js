5b7ac80672a3b803201d4b7d935d31aa82eb7dae
L.W.L <102495553@qq.com>
2019-08-09 17:08:52 +0800

    add color

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const styleObj = {
+  color: 'red'
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Jenkins</h1>
+      <h1 style={styleObj}>Hello Jenkins</h1>
       <p>我叫布罗利</p>
     </div>
   );
